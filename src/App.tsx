01125c96c5c670e8dc0a573ca0011c943e699d71
polux0 <afidelo@protonmail.com>
2023-02-08 17:23:57 +0100

    feature: `settings` dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -76,21 +76,20 @@ function App() {
               ><img src={dropDownSvg} style={imageStyle}></img></button>
               {/* Settings dropdown menu */}
               <div id="dropdown" ref={settingsPopoverDropdownRef} className={(settingsDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
-                "absolute text-base z-10 float-right w-1/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg mt-40 ml-36"
+                "absolute text-base z-10 float-right w-1/6 h-2/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg p-5 mt-40 ml-36"
               }>
-                <h3>Choose slippage</h3>
-                <div className="flex flex-col py-2 text-sm dark:text-gray-400 p-5">
-                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
-                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
-                    <div className="w-3/4 float-left mr-9"> $FRAX </div>
+                <h3>Slippage tolerance</h3>
+                <div className="flex flex-col h-4/5 py-2 text-sm dark:text-gray-400">
+                  <div className="flex justify-center items-center w-6/6 h-1/3 bg-white">
+                    <button className="w-1/4 h-full rounded-2xl border-2 border-gray mr-1">
+                        Auto
+                    </button>
+                    <input className="w-3/4 text-2xl text-white p-4 rounded-2xl focus:outline-0" value={"0,5 %"}style={{backgroundColor}}></input>
                   </div>
-                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
-                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
-                    <div className="w-3/4 float-left mr-9"> $USDC </div>
-                  </div>
-                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
-                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
-                    <div className="w-3/4 float-left mr-9"> $USDT </div>
+                  <div className="w-6/6 h-full p-2 bg-white">
+                    <div className="w-4/4 text-1xl">
+                       Your transaction will revert if the price changes unfavorably by more than this percentage during your order.
+                    </div>
                   </div>
                 </div>
               </div>
