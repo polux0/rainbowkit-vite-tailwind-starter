246b679bb9f91e9c715426609828d4cc7974596c
polux0 <afidelo@protonmail.com>
2023-02-08 12:14:50 +0100

    fix: select currency dropdown modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import YourSvg from "./assets/noun-settings-pixel-art-2758641.svg";
 import usdcSvg from "./assets/usdc-logo-svg.svg"
+import usdcDarkSvg from "./assets/usdc-logo-dark-svg.svg"
 import dropDownSvg from "./assets/dropdown-indicator.svg"
 import React from "react";
 import { createPopper } from "@popperjs/core";
 
 function App() {
 
-  // technical debt - convert styles to tailwind;
+  // technical debt:
+  // convert styles to tailwind;
+  // popoverDropdownRef has position absolute and huge margins - see if there is better way of approaching this
   const backgroundColor = "#1A1B23";
   const imageStyle = {
     display: "block",
@@ -31,28 +34,6 @@ function App() {
   return (
     
     <div className="h-screen bg-black p-6" style={{backgroundColor}}>
-
-      {/* AnotherDropdown menu */}
-      {/* <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-            <div className="py-1" role="none">
-              <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-              <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Edit</a>
-              <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Duplicate</a>
-            </div>
-            <div className="py-1" role="none">
-              <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Archive</a>
-              <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Move</a>
-            </div>
-            <div className="py-1" role="none">
-              <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-4">Share</a>
-              <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-5">Add to favorites</a>
-            </div>
-            <div className="py-1" role="none">
-              <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-6">Delete</a>
-            </div>
-          </div>
-          </div> */}
-
       <div className="h-3/5 w-2/6 bg-white rounded-3xl p-6">
         <div className = "w-full h-1/6">
           <button className="float-right"><img src={YourSvg}></img></button>
@@ -74,27 +55,24 @@ function App() {
               <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown"><img className="float-right"src={usdcSvg}></img></button>
               <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown"><img src={dropDownSvg} style={imageStyle}></img></button>
               {/* Dropdown menu */}
-              {/* Was before */}
               <div id="dropdown" ref={popoverDropdownRef} className={(dropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
-                "absolute text-base z-10 float-right w-1/5 py-2 list-none text-left rounded shadow-lg mt-1"
+                "absolute text-base z-10 float-right w-1/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg mt-40 ml-36"
               }>
-              {/* <div id="dropdown" ref={popoverDropdownRef} className={(dropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
-                "z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-              }> */}
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                  <li>
-                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                  </li>
-                </ul>
+                <h3>Choose token</h3>
+                <div className="flex flex-col py-2 text-sm dark:text-gray-400 p-5">
+                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                    <div className="w-3/4 float-left mr-9"> $FRAX </div>
+                  </div>
+                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                    <div className="w-3/4 float-left mr-9"> $USDC </div>
+                  </div>
+                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                    <div className="w-3/4 float-left mr-9"> $USDT </div>
+                  </div>
+                </div>
           </div>
           </div>
           <div className="w-5/6 h-2/6 rounded-2xl border-4 border-black border-solid">
@@ -103,8 +81,6 @@ function App() {
         </div>
       </div>
     </div>
-    
-    // Dropdown
     
   );
 }
