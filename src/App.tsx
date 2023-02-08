2b054a5499f05d731092444b82eef0986350cb05
polux0 <afidelo@protonmail.com>
2023-02-08 14:57:08 +0100

    feature: outlines of `settings` dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import YourSvg from "./assets/noun-settings-pixel-art-2758641.svg";
+import settingsSvg from "./assets/noun-settings-pixel-art-2758641.svg";
 import usdcSvg from "./assets/usdc-logo-svg.svg"
 import usdcDarkSvg from "./assets/usdc-logo-dark-svg.svg"
 import dropDownSvg from "./assets/dropdown-indicator.svg"
@@ -9,7 +9,7 @@ function App() {
 
   // technical debt:
   // convert styles to tailwind;
-  // popoverDropdownRef has position absolute and huge margins - see if there is better way of approaching this
+  // currenciesPopoverDropdownRef has position absolute and huge margins - see if there is better way of approaching this
   const backgroundColor = "#1A1B23";
   const imageStyle = {
     display: "block",
@@ -19,43 +19,89 @@ function App() {
   const color = "white";
   const backgroundColor1 = "yellow";
   
-  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
-  const buttonDropdownRef: any = React.createRef();
-  const popoverDropdownRef: any = React.createRef();
-  const openDropdownPopover = () => {
-    createPopper(buttonDropdownRef.current, popoverDropdownRef.current, {
+  // currencies dropdown
+  const [currenciesDropdownPopoverShow, setCurrenciesDropdownPopoverShow] = React.useState(false);
+  const currenciesButtonDropdownRef: any = React.createRef();
+  const currenciesPopoverDropdownRef: any = React.createRef();
+  // settings dropdown
+  const [settingsDropdownPopoverShow, setSettingsDropdownPopoverShow] = React.useState(false);
+  const settingsButtonDropdownPopoverShow: any = React.createRef();
+  const settingsPopoverDropdownRef: any = React.createRef();
+
+  // open currencies dropdown
+  const openCurrenciesDropdownPopover = () => {
+    createPopper(currenciesButtonDropdownRef.current, currenciesPopoverDropdownRef.current, {
       placement: "bottom-start"
     });
-    setDropdownPopoverShow(true);
+    setCurrenciesDropdownPopoverShow(true);
   };
-  const closeDropdownPopover = () => {
-    setDropdownPopoverShow(false);
+  // close currencies dropdown
+  const closeCurrenciesDropdownPopover = () => {
+    setCurrenciesDropdownPopoverShow(false);
   };
+
+  // open settings dropdown
+  const openSettingsDropdownPopover = () => {
+    createPopper(settingsButtonDropdownPopoverShow.current, settingsPopoverDropdownRef.current, {
+      placement: "bottom-start"
+    });
+    setSettingsDropdownPopoverShow(true);
+  };
+  // close settings dropdown
+  const closeSettingsDropdownPopover = () => {
+    setSettingsDropdownPopoverShow(false);
+  };
+
   return (
     
     <div className="h-screen bg-black p-6" style={{backgroundColor}}>
       <div className="h-3/5 w-2/6 bg-white rounded-3xl p-6">
         <div className = "w-full h-1/6">
-          <button className="float-right"><img src={YourSvg}></img></button>
+          <button className="float-right" onClick={() => {
+                settingsDropdownPopoverShow
+                  ? closeSettingsDropdownPopover()
+                  : openSettingsDropdownPopover();
+              }}><img src={settingsSvg}></img></button>
         </div>
         {/* Swap */}
         <div className="flex flex-col justify-center items-center space-y-2 h-2/3">
           <div className="flex justify-center items-center w-5/6 h-2/6 rounded-2xl" style={{backgroundColor}}>
               <input className="w-2/3 h-2/3 text-4xl text-white p-4 focus:outline-0" style={{backgroundColor}}></input>
-              <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown"><img className="float-right"src={usdcSvg}></img></button>
-              <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown" onClick={() => {
-                dropdownPopoverShow
-                  ? closeDropdownPopover()
-                  : openDropdownPopover();
+              <button className="w-1/6 h-1/2"><img className="float-right"src={usdcSvg}></img></button>
+              <button className="w-1/6 h-1/2" onClick={() => {
+                currenciesDropdownPopoverShow
+                  ? closeCurrenciesDropdownPopover()
+                  : openCurrenciesDropdownPopover();
               }}
               ><img src={dropDownSvg} style={imageStyle}></img></button>
+              {/* Settings dropdown menu */}
+              <div id="dropdown" ref={settingsPopoverDropdownRef} className={(settingsDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
+                "absolute text-base z-10 float-right w-1/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg mt-40 ml-36"
+              }>
+                <h3>Choose slippage</h3>
+                <div className="flex flex-col py-2 text-sm dark:text-gray-400 p-5">
+                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                    <div className="w-3/4 float-left mr-9"> $FRAX </div>
+                  </div>
+                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                    <div className="w-3/4 float-left mr-9"> $USDC </div>
+                  </div>
+                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                    <div className="w-3/4 float-left mr-9"> $USDT </div>
+                  </div>
+                </div>
+              </div>
+              {/* Settings dropdown menu */}
           </div>
           <div className="flex justify-center items-center w-5/6 h-2/6 rounded-2xl" style={{backgroundColor}}>
               <input className="w-2/3 h-2/3 text-4xl text-white p-4 focus:outline-0" style={{backgroundColor}}></input>
               <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown"><img className="float-right"src={usdcSvg}></img></button>
               <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown"><img src={dropDownSvg} style={imageStyle}></img></button>
-              {/* Dropdown menu */}
-              <div id="dropdown" ref={popoverDropdownRef} className={(dropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
+              {/* Currencies dropdown menu */}
+              <div id="dropdown" ref={currenciesPopoverDropdownRef} className={(currenciesDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
                 "absolute text-base z-10 float-right w-1/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg mt-40 ml-36"
               }>
                 <h3>Choose token</h3>
@@ -73,7 +119,8 @@ function App() {
                     <div className="w-3/4 float-left mr-9"> $USDT </div>
                   </div>
                 </div>
-          </div>
+              </div>
+              {/* Currencies dropdown menu */}
           </div>
           <div className="w-5/6 h-2/6 rounded-2xl border-4 border-black border-solid">
             <button className="w-full bg-white h-full rounded-2xl">Swap</button>
