45dfa7203bf1351d0c447d1af9feb010bcd2705f
polux0 <afidelo@protonmail.com>
2023-02-10 18:45:32 +0100

    fix: exchange modal centered ( although fixed, so this is technical debt

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,14 @@ function App() {
   // convert styles to tailwind;
   // currenciesPopoverDropdownRef has position absolute and huge margins - see if there is better way of approaching this
   // different values are fixed and ( should be dynamic )
+  // swap container has a fixed height ( 500px )
   const backgroundColor = "#1A1B23";
   const imageStyle = {
     display: "block",
     marginLeft: "auto",
     marginRight: "auto"
   };
+  const exchangeContainerHeight = {height: "500px"};
   const color = "white";
   const backgroundColor1 = "yellow";
   
@@ -72,109 +74,109 @@ function App() {
   };
 
   return (
-    
     <div className="h-screen bg-black p-6" style={{backgroundColor}}>
-      <div className="h-3/5 w-2/6 bg-white rounded-3xl p-6">
-        <div className = "w-full h-1/6">
-          <button className="float-right" onClick={() => {
-                settingsDropdownPopoverShow
-                  ? closeSettingsDropdownPopover()
-                  : openSettingsDropdownPopover();
-              }}><img src={settingsSvg}></img></button>
-        </div>
-        {/* Swap */}
-        <div className="flex flex-col justify-center items-center space-y-2 h-2/3">
-          <div className="flex justify-center items-center w-5/6 h-2/6 rounded-2xl" style={{backgroundColor}}>
-              <input className="w-2/3 h-2/3 text-4xl text-white p-4 focus:outline-0" style={{backgroundColor}}></input>
-              <button className="w-1/6 h-1/2"><img className="float-right"src={usdcSvg}></img></button>
-              <button className="w-1/6 h-1/2" onClick={() => {
-                currenciesDropdownPopoverShow
-                  ? closeCurrenciesDropdownPopover()
-                  : openCurrenciesDropdownPopover();
-              }}
-              ><img src={dropDownSvg} style={imageStyle}></img></button>
-              {/* Settings dropdown */}
-              <div id="dropdown" ref={settingsPopoverDropdownRef} className={(settingsDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
-                "absolute text-base z-10 float-right w-1/6 h-2/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg p-5 mt-40 ml-36"
-              }>
-                <h3>Slippage tolerance</h3>
-                <div className="flex flex-col h-4/5 py-2 text-sm dark:text-gray-400">
-                  <div className="flex justify-center items-center text-center w-6/6 h-1/3 bg-white">
-                    <button className="w-1/4 h-full rounded-2xl border-2 border-gray mr-1">
-                        Auto
-                    </button>
-                    <input className="w-3/4 text-2xl text-white text-center p-4 rounded-2xl focus:outline-0" value={"0,5 %"}style={{backgroundColor}}></input>
-                  </div>
-                  <div className="w-6/6 h-full p-2 bg-white">
-                    <div className="w-4/4 text-1xl">
-                       Your transaction will revert if the price changes unfavorably by more than this percentage during your order.
+      <div className="flex flex-row w-full min-h-full justify-center items-center">
+        <div className="h-max bg-white rounded-3xl p-6" style={exchangeContainerHeight}>
+          <div className = "w-full h-1/6">
+            <button className="float-right" onClick={() => {
+                  settingsDropdownPopoverShow
+                    ? closeSettingsDropdownPopover()
+                    : openSettingsDropdownPopover();
+                }}><img src={settingsSvg}></img></button>
+          </div>
+          {/* Swap */}
+          <div className="flex flex-col justify-center items-center space-y-2 h-2/3">
+            <div className="flex justify-center items-center w-5/6 h-2/6 rounded-2xl" style={{backgroundColor}}>
+                <input className="w-2/3 h-2/3 text-4xl text-white p-4 focus:outline-0" style={{backgroundColor}}></input>
+                <button className="w-1/6 h-1/2"><img className="float-right"src={usdcSvg}></img></button>
+                <button className="w-1/6 h-1/2" onClick={() => {
+                  currenciesDropdownPopoverShow
+                    ? closeCurrenciesDropdownPopover()
+                    : openCurrenciesDropdownPopover();
+                }}
+                ><img src={dropDownSvg} style={imageStyle}></img></button>
+                {/* Settings dropdown */}
+                <div id="dropdown" ref={settingsPopoverDropdownRef} className={(settingsDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
+                  "absolute text-base z-10 float-right w-1/6 h-2/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg p-5 mt-40 ml-36"
+                }>
+                  <h3>Slippage tolerance</h3>
+                  <div className="flex flex-col h-4/5 py-2 text-sm dark:text-gray-400">
+                    <div className="flex justify-center items-center text-center w-6/6 h-1/3 bg-white">
+                      <button className="w-1/4 h-full rounded-2xl border-2 border-gray mr-1">
+                          Auto
+                      </button>
+                      <input className="w-3/4 text-2xl text-white text-center p-4 rounded-2xl focus:outline-0" value={"0,5 %"}style={{backgroundColor}}></input>
+                    </div>
+                    <div className="w-6/6 h-full p-2 bg-white">
+                      <div className="w-4/4 text-1xl">
+                        Your transaction will revert if the price changes unfavorably by more than this percentage during your order.
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              {/* Settings dropdown */}
-          </div>
-          <div className="flex justify-center items-center w-5/6 h-2/6 rounded-2xl" style={{backgroundColor}}>
-              <input className="w-2/3 h-2/3 text-4xl text-white p-4 focus:outline-0" style={{backgroundColor}}></input>
-              <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown"><img className="float-right"src={usdcSvg}></img></button>
-              <button className="w-1/6 h-1/9" data-dropdown-toggle="dropdown" onClick={() => {
-                additionalTradeInfoDropdownPopoverShow
-                  ? closeAdditionalTradeInfoDropdownPopover()
-                  : openAdditionalTradeInfoDropdownPopover();
-              }}><img src={informationIndicatorWhite} style={imageStyle}></img></button>
-              {/* Currencies dropdown */}
-              <div id="dropdown" ref={currenciesPopoverDropdownRef} className={(currenciesDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
-                "absolute text-base z-10 float-right w-1/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg mt-40 ml-36"
-              }>
-                <h3>Choose token</h3>
-                <div className="flex flex-col py-2 text-sm dark:text-gray-400 p-5">
-                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
-                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
-                    <div className="w-3/4 float-left mr-9"> $FRAX </div>
-                  </div>
-                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
-                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
-                    <div className="w-3/4 float-left mr-9"> $USDC </div>
-                  </div>
-                  <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
-                    <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
-                    <div className="w-3/4 float-left mr-9"> $USDT </div>
+                {/* Settings dropdown */}
+            </div>
+            <div className="flex justify-center items-center w-5/6 h-2/6 rounded-2xl" style={{backgroundColor}}>
+                <input className="w-2/3 h-2/3 text-4xl text-white p-4 focus:outline-0" style={{backgroundColor}}></input>
+                <button className="w-1/6 h-1/2" data-dropdown-toggle="dropdown"><img className="float-right"src={usdcSvg}></img></button>
+                <button className="w-1/6 h-1/9" data-dropdown-toggle="dropdown" onClick={() => {
+                  additionalTradeInfoDropdownPopoverShow
+                    ? closeAdditionalTradeInfoDropdownPopover()
+                    : openAdditionalTradeInfoDropdownPopover();
+                }}><img src={informationIndicatorWhite} style={imageStyle}></img></button>
+                {/* Currencies dropdown */}
+                <div id="dropdown" ref={currenciesPopoverDropdownRef} className={(currenciesDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
+                  "absolute text-base z-10 float-right w-1/6 py-2 list-none text-center rounded-2xl border-4 border-black border-solid shadow-lg mt-40 ml-36"
+                }>
+                  <h3>Choose token</h3>
+                  <div className="flex flex-col py-2 text-sm dark:text-gray-400 p-5">
+                    <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                      <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                      <div className="w-3/4 float-left mr-9"> $FRAX </div>
+                    </div>
+                    <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                      <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                      <div className="w-3/4 float-left mr-9"> $USDC </div>
+                    </div>
+                    <div className="flex justify-center items-center w-6/6 h-1/9 p-2 rounded-2xl border-2 border-gray hover:text-black hover:border-black border-solid mb-2.5 bg-white">
+                      <button className="w-1/4"><img src={usdcDarkSvg}></img></button>
+                      <div className="w-3/4 float-left mr-9"> $USDT </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              {/* Currencies dropdown */}
-              {/* Additional trade info dropdown */}
-              <div id="dropdown" ref={additionalTradeInfoPopoverDropdownRef} className={(additionalTradeInfoDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
-                "absolute text-base z-10 float-right h-2/6 w-1/5 py-2 list-none rounded-2xl border-4 border-black border-solid shadow-lg mx-0	my-0 p-5"
-              }>
-                <div className="flex flex-col py-6 text-sm dark:text-gray-400 p-4">
-                  <div className="mb-6">
-                    <h3 className="float-left text-lg text-black">More details</h3>
-                    <h3 className="float-right text-lg text-black" onClick={() => {  if(additionalTradeInfoDropdownPopoverShow){closeAdditionalTradeInfoDropdownPopover()}}}>X</h3>
+                {/* Currencies dropdown */}
+                {/* Additional trade info dropdown */}
+                <div id="dropdown" ref={additionalTradeInfoPopoverDropdownRef} className={(additionalTradeInfoDropdownPopoverShow ? "block " : "hidden ") + (color === "white" ? "bg-white " : backgroundColor1 + " ") +
+                  "absolute text-base z-10 float-right h-2/6 w-1/5 py-2 list-none rounded-2xl border-4 border-black border-solid shadow-lg mx-0	my-0 p-5"
+                }>
+                  <div className="flex flex-col space-y-2 py-4 text-sm dark:text-gray-400 p-2">
+                    <div className="mb-6">
+                      <h3 className="float-left text-lg text-black">More details</h3>
+                      <h3 className="float-right text-lg text-black" onClick={() => {  if(additionalTradeInfoDropdownPopoverShow){closeAdditionalTradeInfoDropdownPopover()}}}>X</h3>
+                    </div>
+                    <div className="float-left"><h2 className="text-black">Expected output</h2></div>
+                    <div className="float-left"><h2>12.0798 $AGENCY</h2></div>
+                    <div className="float-left"><h2 className="text-black">Price impact</h2></div>
+                    <div className="float-left"><h2>0%</h2></div>
+                    <div className="float-left"><h2 className="text-black">Minimum received after slippage ( %0.5 )</h2></div>
+                    <div className="float-left"><h2>12.0798 $AGENCY</h2></div>
+                    <div className="float-left"><h2 className="text-black">Network fee</h2></div>
+                    <div className="float-left"><h2>~$1.72</h2></div>
+                    {/* <div className="flex justify-left items-left w-6/6 h-1/9 p-2">
+                      <div className="w-4/4">Network fee</div>
+                      <div className="w-1/4">~$1.72</div>
+                    </div> */}
                   </div>
-                  <div className="float-left"><h2 className="text-black">Expected output</h2></div>
-                  <div className="float-left"><h2>12.0798 $AGENCY</h2></div>
-                  <div className="float-left"><h2 className="text-black">Price impact</h2></div>
-                  <div className="float-left"><h2>0%</h2></div>
-                  <div className="float-left"><h2 className="text-black">Minimum received after slippage ( %0.5 )</h2></div>
-                  <div className="float-left"><h2>12.0798 $AGENCY</h2></div>
-                  <div className="float-left"><h2 className="text-black">Network fee</h2></div>
-                  <div className="float-left"><h2>~$1.72</h2></div>
-                  {/* <div className="flex justify-left items-left w-6/6 h-1/9 p-2">
-                    <div className="w-4/4">Network fee</div>
-                    <div className="w-1/4">~$1.72</div>
-                  </div> */}
                 </div>
-              </div>
+            </div>
+            {/* <button className="w-1/6 h-1/9" data-dropdown-toggle="dropdown"><img src={informationIndicator} style={imageStyle}></img></button> */}
+            <div className="w-5/6 h-2/6 rounded-2xl border-4 border-black border-solid">
+              <button className="w-full bg-white h-full rounded-2xl">Swap</button>
           </div>
-          {/* <button className="w-1/6 h-1/9" data-dropdown-toggle="dropdown"><img src={informationIndicator} style={imageStyle}></img></button> */}
-          <div className="w-5/6 h-2/6 rounded-2xl border-4 border-black border-solid">
-            <button className="w-full bg-white h-full rounded-2xl">Swap</button>
-        </div>
+          </div>
         </div>
       </div>
     </div>
-    
   );
 }
 
