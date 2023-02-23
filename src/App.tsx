5e9f05b424f1f0a43ca56d6f69c59674f5938faf
polux0 <afidelo@protonmail.com>
2023-02-23 11:06:32 +0100

    fix: added `cursor-pointer` for image that closes tradeInfo modal

--- src/App.tsx
+++ src/App.tsx
@@ -175,7 +175,7 @@ function App() {
                   <div className="flex flex-col space-y-2 py-4 text-sm dark:text-gray-400 p-2">
                     <div className="mb-6">
                       <h3 className="float-left text-lg text-black">More details</h3>
-                      <h3 className="float-right text-lg text-black" onClick={() => {  if(additionalTradeInfoDropdownPopoverShow){closeAdditionalTradeInfoDropdownPopover()}}}>X</h3>
+                      <h3 className="float-right text-lg text-black hover:cursor-pointer" onClick={() => {  if(additionalTradeInfoDropdownPopoverShow){closeAdditionalTradeInfoDropdownPopover()}}}>X</h3>
                     </div>
                     <div className="float-left"><h2 className="text-black">Expected output</h2></div>
                     <div className="float-left"><h2>12.0798 $AGENCY</h2></div>
